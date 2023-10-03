a0758a86f26419ad92873eaaf9c1cede6eb84b07
sukumar <p.sukumara7@gmail.com>
2023-10-03 13:30:13 +0530

    Initial commit2

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import {
+  faPlusCircle,
+  faCaretDown,
+  faHotel,faCar,faUtensilSpoon,faMugHot,faPersonBooth,faCloud,faCalendar,faStar
+
+} from "@fortawesome/free-solid-svg-icons";
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'classimax';
+  faPlus=faPlusCircle;
+  faCartArrowDown=faCaretDown;
+  faHotel= faHotel;
+  faCar =  faCar;
+  faUtensilSpoon=faUtensilSpoon;faMugHot=faMugHot;faPersonBooth=faPersonBooth;faCloud=faCloud;faCalendar=faCalendar;faStar=faStar
+
+}
